d5633344cfbd3361193ec86d083b8ae23c515a14
oodukudu <oyindamola_oluwaseun@yahoo.com>
2018-07-09 21:49:41 +0100

    Add files via upload

---
+++ src/test/java/StepDefinitions/WishList.java
@@ -0,0 +1,45 @@
+package StepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class WishList {
+    @Given("^I navigate to giftrete site$")
+    public void i_navigate_to_giftrete_site() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new PendingException();
+    }
+
+    @And("^I click on wishlist$")
+    public void iClickOnWishlist() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^i click on create wishlist$")
+    public void iClickOnCreateWishlist() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^i fill in all the necessary information$")
+    public void iFillInAllTheNecessaryInformation() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^i click save$")
+    public void iClickSave() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^my wishlist should be created$")
+    public void myWishlistShouldBeCreated() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
